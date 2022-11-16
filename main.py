a4a6c50b063910cb6d05622037bee24f299266bc
demsogit <54018134+Kir555@users.noreply.github.com>
2022-11-16 22:31:33 +0300

    first

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('idi na huy')
+    print_hi('idi na hu1y')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
